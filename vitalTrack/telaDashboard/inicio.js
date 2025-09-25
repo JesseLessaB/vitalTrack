7b940f7bec0509aa2f31295bbb79ec6a7295910b
JesseLessaB <lessaadv7@gmail.com>
2025-09-25 16:42:54 -0300

    login concluido

--- vitalTrack/telaDashboard/inicio.js
+++ vitalTrack/telaDashboard/inicio.js
@@ -1,9 +1,17 @@
 // Verifica se está logado
 if (localStorage.getItem("logado") !== "true") {
     window.location.href = "../telalogin/login.html";
+  } else {
+    // Recupera o usuário salvo
+    const usuarioSalvo = JSON.parse(localStorage.getItem("usuario"));
+    const msgUsuario = document.getElementById("msgUsuario");
+  
+    if (usuarioSalvo && usuarioSalvo.nome) {
+      msgUsuario.textContent = `Olá, ${usuarioSalvo.nome}!`;
+    }
   }
   
-
+  // Botão de logout
   document.getElementById("btnPerfil").addEventListener("click", function () {
     if (confirm("Deseja sair da sua conta?")) {
       localStorage.removeItem("logado");
